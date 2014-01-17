3104e32921fba2401101bb00f022a89f52b2352b
Xander Masotto <xmasotto@gmail.com>
2014-01-17 04:25:27 -0600

    hey3

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import xandroid
-android = xandroid.android()
 
-droid = android.Android()
+droid = xandroid.Android()
 message = droid.dialogGetInput("TTS", "What would you like to say?").result
 droid.ttsSpeak(message)
--- xandroid.py
+++ xandroid.py
@@ -1,21 +1,17 @@
-def android():
-    return Mocker("android")
+def Android():
+    try:
+        import android
+        return android.Android()
+    else:
+        return Mocker("android.Android()")
 
 class Mocker():
     def __init__(self, name):
         self.name = name
-        try:
-            import android
-            self.has_android = True
-        except:
-            self.has_android = False
 
     def __getattr__(self, name):
-        if self.has_android:
-            return android.__getattr__(name)
-        else:
-            newname = self.name + "." + name
-            return Mocker(newname)
+        newname = self.name + "." + name
+        return Mocker(newname)
 
     def __call__(self, *args):
         newname = self.name + "()"
