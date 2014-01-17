85a1f1febc2727589856f9b5fafbd9cacd69d3bd
Xander Masotto <xmasotto@gmail.com>
2014-01-16 21:57:25 -0600

    ab

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import subprocess
 
 from subprocess import Popen, PIPE, STDOUT
 
-p = Popen(['sqlite3'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+p = Popen(['/system/bin/sqlite3'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
 command = 'hello'
 print("Command: " + command)
 print("Result: \n" + p.communicate(input=command+";")[0])
