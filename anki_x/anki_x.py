fb1ebaa3f7d7e5e8c699bf55de83a3d13fe6832c
Xander Masotto <xmasotto@gmail.com>
2014-01-18 18:39:14 -0600

    hi14

--- anki_x/anki_x.py
+++ anki_x/anki_x.py
@@ -60,7 +60,8 @@ def deck_main(deckname, new):
             insert_card(deck, line, deck_data)
 
     # fix corrupted decks
-    print(deck_data['nid2did'])
+    print(deck_data['nid2did'].keys())
+    print(anki_db.get_default_cards())
     for nid in anki_db.get_default_cards():
         if nid in deck_data['nid2did']:
             anki_db.fix_deck_id(
